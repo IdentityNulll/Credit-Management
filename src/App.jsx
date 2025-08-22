b6262debe36b58dcaef05921a446f3a5b323a835
IdentityNull <identitynulll@gmail.com>
2025-08-22 08:08:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,6 @@ import Home from './Pages/Home/Home.jsx'
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    if (window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp;
-      tg.ready();
-      tg.expand();
-      if (tg.initDataUnsafe?.user) {
-        setUsername(tg.initDataUnsafe.user.first_name);
-      }
-    }
-  }, []);
   return (
     <div>
       <Home />  
