a81953ff571fd905f613aa427cdf79bc53e6c0c4
IdentityNull <identitynulll@gmail.com>
2025-08-22 08:25:04 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Home from './Pages/Home/Home.jsx'
-import React, { useEffect, useState } from "react";
+import Home from './Pages/Home/Home.jsx
 
 function App() {
   return (
