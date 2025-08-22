e675ca4c3a2f7a8694bae25167f64d64d11a592a
Identitynull <identitynulll@gmail.com>
2025-08-22 08:50:49 +0500

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Home from './Pages/Home/Home.jsx'
+import React from "react";
+import Home from "./Pages/Home/Home.jsx";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+
+      tg.ready();   // notify Telegram we're ready
+      tg.expand();  // make it fullscreen
+
+      // Get user info if available
+      console.log("hello")
+      if (tg.initDataUnsafe?.user) {
+        setUsername(tg.initDataUnsafe.user.first_name);
+      }
+    } else {
+      console.log("Telegram WebApp API not found");
+    }
+  }, []);
   return (
     <div>
-      <Home />  
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
