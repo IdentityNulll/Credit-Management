4c3f9318d5f686318f5c46747936d163219f0a0c
IdentityNull <identitynulll@gmail.com>
2025-08-22 09:08:46 +0500

    Bruhhhhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import Home from './Pages/Home/Home.jsx'
-import {useState, useEffect} from 'react' 
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    if (window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp;
-
-      tg.ready();   // notify Telegram we're ready
-      tg.expand();  // make it fullscreen
-
-      // Get user info if available
-      console.log("hello")
-      if (tg.initDataUnsafe?.user) {
-        setUsername(tg.initDataUnsafe.user.first_name);
-      }
-    } else {
-      console.log("Telegram WebApp API not found");
-    }
-  }, []);
   return (
     <div>
       <Home />
