0172bc9802d370ee5935f4a2cb9498f861478bdb
IdentityNull <identitynulll@gmail.com>
2025-08-22 07:53:47 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import Home from './Pages/Home/Home.jsx'
 
 function App() {
+    useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+      tg.ready();
+      tg.expand();
+      console.log("Telegram initialized", tg.initDataUnsafe);
+    } else {
+      console.log("Telegram WebApp API not found");
+    }
+  }, []);
   return (
     <div>
       <Home />  
